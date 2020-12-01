24e7130840f17f1d06e8d1ba34d740db8929c420
ark893 <anishkhatavkar@gmail.com>
2020-12-02 00:10:03 +0530

    Initial Commit

---
+++ src/pages/MovieDesc.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import api from '../api'
+import './MovieDesc.css'
+
+export default class MovieDesc extends Component{
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id: this.props.match.params.id,
+            name: '',
+            comments: '',
+            duration: '',
+        }
+    }
+
+    componentDidMount = async () => {
+        const { id } = this.state
+        const movie = await api.getMovieById(id)
+
+        this.setState({
+            name: movie.data.data.name,
+            comments: movie.data.data.comments,
+            duration: movie.data.data.duration
+        })
+    }
+
+    render(){
+        const {name, comments, duration} = this.state;
+        console.log(name, comments, duration);
+        return(
+            <div>
+            {
+                <div className='DescHold'>
+                    {
+                    'Description: '
+                        + comments
+                    }
+                </div>
+            }
+            </div>
+        )
+    }
+}
+
